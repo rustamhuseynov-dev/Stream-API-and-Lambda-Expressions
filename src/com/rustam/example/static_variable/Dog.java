cc667aa8878c400049eb607536fe6d0b008363ed
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-03 09:51:22 +0400

    Create Dog.java

---
+++ src/com/rustam/example/static_variable/Dog.java
@@ -0,0 +1,14 @@
+package com.rustam.example.static_variable;
+
+public class Dog extends Animal {
+
+    public Dog(String name){
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
