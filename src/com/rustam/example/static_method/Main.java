04361a168874ecbe50a4504338b17d2368197e14
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 14:03:22 +0400

    Create Main.java

---
+++ src/com/rustam/example/static_method/Main.java
@@ -0,0 +1,8 @@
+package com.rustam.example.static_method;
+
+public class Main {
+    public static void main(String[] args) {
+        String salam = Person.name("Salam");
+        System.out.println(salam);
+    }
+}
